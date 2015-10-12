6311d55ef15c0f05938831ceae61d6eced625a6f
Adam Steel <adamgsteel@gmail.com>
2015-10-12 12:00:10 -0400

    [syntax] add spaces to expectations

--- spec/shared_behavior/an_object_spec.rb
+++ spec/shared_behavior/an_object_spec.rb
@@ -28,6 +28,7 @@ shared_examples 'an object' do |callback|
   after(:each) do
     HashDot.universal_dot_syntax = false
   end
+
   it 'allows hashes to access nested hashes' do
     expect( user.address.city ).to eq( user[:address][:city] )
   end
@@ -36,8 +37,8 @@ shared_examples 'an object' do |callback|
     user.address.city = "White Plains"
     user.name = "New Name"
 
-    expect( user.address.city ).to eq("White Plains")
-    expect( user.name ).to eq("New Name")
+    expect( user.address.city ).to eq( "White Plains" )
+    expect( user.name ).to eq( "New Name" )
   end
 
   it 'handles JSON parsing of json strings' do
@@ -46,12 +47,12 @@ shared_examples 'an object' do |callback|
     json_user.address.city = "White Plains"
     json_user.name = "New Name"
 
-    expect( json_user.address.city ).to eq("White Plains")
-    expect( json_user.name ).to eq("New Name")
+    expect( json_user.address.city ).to eq( "White Plains" )
+    expect( json_user.name ).to eq( "New Name" )
   end
 
   it 'defaults to expected behavior when non existent methods are applied' do
-    expect { json_user.non_existent_method }.to raise_error(NoMethodError)
-    expect { user.non_existent_method }.to raise_error(NoMethodError)
+    expect { json_user.non_existent_method }.to raise_error( NoMethodError )
+    expect { user.non_existent_method }.to raise_error( NoMethodError )
   end
 end
