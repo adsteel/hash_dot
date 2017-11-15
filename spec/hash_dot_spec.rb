0d007f341b49d93d13e318f9d3af8bd5d5d4984e
Dave Steinberg <dave@steinbergcomputing.com>
2017-11-15 02:24:47 +0000

    revise tests to expand the permutations related to use_default

--- spec/hash_dot_spec.rb
+++ spec/hash_dot_spec.rb
@@ -10,6 +10,13 @@ describe "Hash dot syntax" do
 
   context "given universal hash-dot syntax" do
     it_behaves_like "an object", -> { Hash.use_dot_syntax = true }
+
+    context "when respecting defaults" do
+      it_behaves_like "an object", -> {
+        Hash.use_dot_syntax = true
+        Hash.hash_dot_use_default = true
+      }
+    end
   end
 
   context "when using #to_dot" do
@@ -37,9 +44,26 @@ describe "Hash dot syntax" do
       expect( two.respond_to?(:a) ).to be_falsey
     end
 
+    it "raises an error if the property has been deleted" do
+      one = { a: 1 }.to_dot
+      expect( one.a ).to eq( 1 )
+      one.delete(:a)
+      expect{ one.a }.to raise_error( NoMethodError )
+    end
+
+    it "defaults to expected behavior when non existent methods are applied" do
+      one = { a: 1 }.to_dot
+      two = { a: 2 }
+
+      expect { one.non_existent_method }.to raise_error( NoMethodError )
+      expect { two.non_existent_method }.to raise_error( NoMethodError )
+    end
+
     it_behaves_like "an object", -> { { action: :to_dot } }
 
-    context "with a use_default set" do
+    context "when respecting defaults" do
+      it_behaves_like "an object", -> { { action: :to_dot, args: {use_default: true} } }
+
       it "uses the hash default for unknown methods" do
         one = { }.to_dot(use_default: true)
         two = { }.to_dot
--- spec/shared_behavior/an_object_spec.rb
+++ spec/shared_behavior/an_object_spec.rb
@@ -25,12 +25,15 @@ shared_examples "an object" do |callback|
     result = callback.call
 
     if result.is_a?(Hash) && result[:action]
-      user.send(result[:action])
-      json_user.send(result[:action])
+      user.send(result[:action], result[:args] || {})
+      json_user.send(result[:action], result[:args] || {})
     end
   end
 
-  after(:each) { Hash.use_dot_syntax = false }
+  after(:each) {
+    Hash.use_dot_syntax = false
+    Hash.hash_dot_use_default = false
+  }
 
   it "allows hashes to access nested hashes" do
     expect( user.address.city ).to eq( user[:address][:city] )
@@ -51,12 +54,6 @@ shared_examples "an object" do |callback|
     expect( user.suffix ).to eq( "Esquire" )
   end
 
-  it "raises an error if the property has been deleted" do
-    user.delete(:email)
-
-    expect { user.email }.to raise_error( NoMethodError )
-  end
-
   it "handles JSON parsing of json strings" do
     expect( json_user.address.city ).to eq( user[:address][:city] )
 
@@ -65,11 +62,6 @@ shared_examples "an object" do |callback|
 
     expect( json_user.address.city ).to eq( "White Plains" )
     expect( json_user.name ).to eq( "New Name" )
-  end
-
-  it "defaults to expected behavior when non existent methods are applied" do
-    expect { json_user.non_existent_method }.to raise_error( NoMethodError )
-    expect { user.non_existent_method }.to raise_error( NoMethodError )
   end
 
   it "recognizes a key with a nil value" do
